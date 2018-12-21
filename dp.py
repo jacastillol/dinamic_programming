b13518945d8daea481153ab6fa2c1a86739a0639
Jaime Andres Castillo Leon <jacastillol@unal.edu.co>
2018-12-21 14:56:31 -0500

    feat: add dp file with declaration of policy evaluation fcn

---
+++ dp.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def policy_evaluation(env, policy):
+    """
+    Policy Evaluation algorithm
+    
+    Eval state-value function using Bellman Expectation equation 
+    """
+    V = np.zeros(env.nS)
+    
+    return V

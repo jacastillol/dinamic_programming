bdf79558f57c212b8d0a9aa168e1e5df0b07a86d
Jaime Andres Castillo Leon <jacastillol@unal.edu.co>
2018-12-21 15:02:09 -0500

    feat: implementation of policy evaluation

--- dp.py
+++ dp.py
@@ -1,11 +1,22 @@
 import numpy as np
 
-def policy_evaluation(env, policy):
+def policy_evaluation(env, policy, gamma=1, theta=1e-8):
     """
     Policy Evaluation algorithm
     
     Eval state-value function using Bellman Expectation equation 
     """
     V = np.zeros(env.nS)
-    
+
+    delta = np.inf
+    while delta > theta:
+        delta = 0
+        for s in range(env.nS):
+            vs = 0
+            for a in env.P[s].keys():
+                probs, next_states, rewards, dones = zip(*env.P[s][a])
+                vs += policy[s][a]*np.sum(probs*(rewards+gamma*V[np.array(next_states)]))
+                delta = max(delta, np.abs(vs-V[s]))
+                V[s] = vs
+
     return V

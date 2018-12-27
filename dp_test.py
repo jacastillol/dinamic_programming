f8d1be34377fe535f5753bddcb2d9df308308a33
Jaime Andres Castillo Leon <jacastillol@unal.edu.co>
2018-12-27 16:45:25 -0500

    test: Add a q_from_v_check test

--- dp_test.py
+++ dp_test.py
@@ -17,6 +17,13 @@ def policy_evaluation_soln(env, policy, gamma=1, theta=1e-8):
             break
     return V
 
+def q_from_v_soln(env, V, s, gamma=1):
+    q = np.zeros(env.nA)
+    for a in range(env.nA):
+        for prob, next_state, reward, done in env.P[s][a]:
+            q[a] += prob * (reward + gamma * V[next_state])
+    return q
+
 env = FrozenLakeEnv()
 random_policy = np.ones([env.nS, env.nA]) / env.nA
 
@@ -25,6 +32,15 @@ class Tests(unittest.TestCase):
     def policy_evaluation_check(self, policy_evaluation):
         soln = policy_evaluation_soln(env, random_policy)
         to_check = policy_evaluation(env, random_policy)
+        np.testing.assert_array_almost_equal(soln, to_check)
+
+    def q_from_v_check(self, q_from_v):
+        V = policy_evaluation_soln(env, random_policy)
+        soln = np.zeros([env.nS, env.nA])
+        to_check = np.zeros([env.nS, env.nA])
+        for s in range(env.nS):
+            soln[s] = q_from_v_soln(env, V, s)
+            to_check[s] = q_from_v(env, V, s)
         np.testing.assert_array_almost_equal(soln, to_check)
 
 check = Tests()

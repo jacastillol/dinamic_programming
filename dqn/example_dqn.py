c17cc091bdf8560f55728100285553c6ec84dc84
Jaime Andres Castillo Leon <jacastillol@unal.edu.co>
2018-12-29 10:10:02 -0500

    feat: add main program

---
+++ dqn/example_dqn.py
@@ -0,0 +1,7 @@
+from dqn_agent import Agent
+from dqn_monitor import dqn_interact
+import gym
+
+env = gym.make('LunarLander-v2')
+agent = Agent(state_size=8, action_size=4, seed=0)
+avg_reward, best_avg_reward = dqn_interact(env, agent)

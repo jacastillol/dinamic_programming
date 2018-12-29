a2ef4a6d1cdbf99517fa34eae8863065bf64d368
Jaime Andres Castillo Leon <jacastillol@unal.edu.co>
2018-12-29 07:29:57 -0500

    feat: Add interface of a posible dqn agent

---
+++ dqn/dqn_agent.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+class Agent():
+    """ This Agent interacts and learn from the environment."""
+    def __init__(self, state_size, action_size, seed):
+        """ Initialize agent object. 
+        """
+
+    def step(self, state, action, reward, next_state, done):
+        """ Interact
+        """
+
+    def act(self, state, eps=0):
+        """ Returns actions for a given state as per current policy
+        """
+
+    def learn(self, experience, gamma):
+        """ Update value parameters using given batch of experience tuples
+        """
+
+

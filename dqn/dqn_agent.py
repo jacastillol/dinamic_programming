b9ed2201f26177afb231841a304ed64ed2410a97
Jaime Andres Castillo Leon <jacastillol@unal.edu.co>
2018-12-29 14:59:18 -0500

    feat: Add Replay Buffer on DQN algorithm
    It was implemented a Replay Buffer on the DQN Agent without a
    target network, after proves the algorithm converges in less than 450
    episodes.

--- dqn/dqn_agent.py
+++ dqn/dqn_agent.py
@@ -1,10 +1,15 @@
 import numpy as np
 from model import Actor
+from collections import deque, namedtuple
 import torch
+import torch.nn.functional as F
 import torch.optim as optim
+import random
 
-GAMMA = 0.99       # discount factor
-LR = 5e-4          # learning rate
+BUFFER_SIZE = int(1e5)  # replay buffer size
+BATCH_SIZE = 64         # minibatch size
+GAMMA = 0.99            # discount factor
+LR = 5e-4               # learning rate
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -26,11 +31,17 @@ class Agent():
         self.actor_local = Actor(self.nS, self.nA, seed).to(device)
         self.optimizer = optim.Adam(self.actor_local.parameters(), lr=LR)
 
+        # Replay memory
+        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed)
+
     def step(self, state, action, reward, next_state, done):
         """ Interact Agent and Environment.
         """
-        experiences = (state, action, reward, next_state, done)
-        self.learn(experiences, GAMMA)
+        self.memory.add(state, action, reward, next_state, done)
+
+        if len(self.memory) > BATCH_SIZE:
+            experiences = self.memory.sample()
+            self.learn(experiences, GAMMA)
 
     def act(self, state, eps=0):
         """ Returns actions for a given state as per current policy.
@@ -63,7 +74,7 @@ class Agent():
         states, actions, rewards, next_states, dones = experiences
 
         # Get max predicted Q values (for next states) from target model
-        Q_target_next = self.actor_local(next_states).detach().max(1)[0].unsqueeze(1)
+        Q_targets_next = self.actor_local(next_states).detach().max(1)[0].unsqueeze(1)
         # Compute Q targets for current states
         Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
 
@@ -77,3 +88,47 @@ class Agent():
         loss.backward()
         self.optimizer.step()
 
+
+class ReplayBuffer:
+    """ Fixed-size buffer to store experience tuples."""
+
+    def __init__(self, action_size, buffer_size, batch_size, seed):
+        """ Initialize a ReplayBuffer object.
+        Params
+        ======
+            action_size (int): dimesion of each action
+            buffer_size (int): maximum size of buffer
+            batch_size (int): size of each training batch
+            seed (int): random seed
+        """
+        self.action_size = action_size
+        self.memory = deque(maxlen=buffer_size)
+        self.batch_size = batch_size
+        self.experience = namedtuple("Experience",
+                                     field_names=["state",
+                                                  "action",
+                                                  "reward",
+                                                  "next_state",
+                                                  "done"])
+        self.seed = seed
+
+    def add(self, state, action, reward, next_state, done):
+        """ Add a new experience to memory."""
+        e = self.experience(state, action, reward, next_state, done)
+        self.memory.append(e)
+
+    def sample(self):
+        """ Randomly sample a batch of expriences from memory. """
+        experiences = random.sample(self.memory, k=self.batch_size)
+
+        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
+        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long().to(device)
+        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
+        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
+        dones =  torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
+
+        return (states, actions, rewards, next_states, dones)
+
+    def __len__(self):
+        """ Return the current size of internal memory. """
+        return len(self.memory)

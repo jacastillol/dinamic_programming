9339d63abd5551750139a2e7971648abcc41efaf
Jaime Andres Castillo Leon <jacastillol@unal.edu.co>
2019-01-06 10:19:51 -0500

    feat: load environment and check properties

--- policy-based/example_cross_entropy.py
+++ policy-based/example_cross_entropy.py
@@ -1,1 +1,12 @@
 import numpy as np
+import gym
+
+env = gym.make('MountainCarContinuous-v0')
+env.seed(0)
+np.random.seed(0)
+
+print('observation space:', env.observation_space)
+print('action space:', env.action_space)
+print('  - low:', env.action_space.low)
+print('  - high:', env.action_space.high)
+
